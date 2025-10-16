26ec584f28a17be4897a2e7b4649f44761a10b54
tracyxoxo <83191716+tracyxoxo@users.noreply.github.com>
2025-10-15 21:09:11 -0300

    Reapply "restructuring directories to support:"
    This reverts commit d4348e2fc85a3473a2b160ee162f22aab627bc95.

--- app/src/main/java/com/example/pokedex/PokedexActivity.java
+++ app/src/main/java/com/example/pokedex/PokedexActivity.java
@@ -15,7 +15,7 @@ import java.io.InputStream;
 import java.net.URL;
 import android.graphics.Bitmap;
 
-public class MainActivity extends AppCompatActivity {
+public class PokedexActivity extends AppCompatActivity {
 
     int posicao = 1;
     int qtde = 10;
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pokedex);
 
         lnlPokedex = findViewById(R.id.lnlPokedex);
 
